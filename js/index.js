cb7d6160ba32fca894a51be2e3fa6ce4df77b03d
muza <muzallyefah@gmail.com>
2023-10-29 23:15:08 +0800

    fix: add footer

--- js/index.js
+++ js/index.js
@@ -79,6 +79,7 @@ $(function(){
     gsap.to('#breaker-two',0,{display:"none",delay:2});
     gsap.to('#about',0,{display:"block",delay:.7});
     gsap.to('#navigation-content',0,{display:'flex',delay:2});
+    gsap.to('#footer',0,{display:"flex",delay:.7});
   })
   $('#portfolio-link').on('click',function(event){
     event.preventDefault();
@@ -92,6 +93,7 @@ $(function(){
     gsap.to('#breaker-two',0,{display:"none",delay:2});
     gsap.to('#portfolio',0,{display:"block",delay:.7});
     gsap.to('#navigation-content',0,{display:'flex',delay:2});
+    gsap.to('#footer',0,{display:"flex",delay:.7});
   })
   $('#home-link').on('click',function(event){
     event.preventDefault();
@@ -106,6 +108,7 @@ $(function(){
     gsap.to('#breaker-two',0,{display:"none",delay:2});
     gsap.to('#header',0,{display:"block",delay:.7});
     gsap.to('#navigation-content',0,{display:'flex',delay:2});
+    gsap.to('#footer',0,{display:"none"});
   })
 
   var body =  document.querySelector('body');
