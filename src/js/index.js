3b3078bf521585efb596a720cf0263be8f13c01a
muza <muzallyefah@gmail.com>
2023-10-30 10:57:45 +0800

    fix: disable loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 $(function(){
-  gsap.to('#loader',1,{y:"-100%"});
-  gsap.to('#loader',1,{opacity:0});
-  gsap.to('#loader',0,{display:"none",delay:1});
-  gsap.to('#navigation-content',0,{display:"none"});
-  gsap.to('#navigation-content',0,{display:"flex",delay:1});
+  // gsap.to('#loader',1,{y:"-100%"});
+  // gsap.to('#loader',1,{opacity:0});
+  // gsap.to('#loader',0,{display:"none",delay:1});
+  // gsap.to('#navigation-content',0,{display:"none"});
+  // gsap.to('#navigation-content',0,{display:"flex",delay:1});
 
   $('.menubar').on('click',function(){
     gsap.to('#navigation-content',.6,{y:0});
